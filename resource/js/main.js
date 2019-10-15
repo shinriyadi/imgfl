ad6ddeb5f8f9dc8cd4abe58fba7ea63dcbe14d4d
shinriyadi <shin.riyadi@gmail.com>
2019-10-15 08:14:25 +0700

    fixed bug font on iphone and positioning element

--- resource/js/main.js
+++ resource/js/main.js
@@ -1,4 +1,15 @@
-$('#menu-nav').click(function () {
-  $('.menu-overlay').toggleClass("active");
-  $(this).toggleClass("active");
+$(document).ready(function () {
+  $('#menu-nav').click(function () {
+    $('.menu-overlay').toggleClass("active");
+    $(this).toggleClass("active");
+  });
+
+  var typed = new Typed('.typed ', {
+    strings: ["/WE CREATE IDEAS <br> CONNECTED BRAND <br> WITH PEOPLE"],
+    typeSpeed: 100,
+    startDelay: 20,
+    loop: true,
+    backSpeed: 40,
+    backDelay: 2000,
+  });
 })
